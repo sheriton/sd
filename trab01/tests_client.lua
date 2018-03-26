c95149d9c32a82bab2d9f7f9229b5d031bb4d722
sheriton <sheriton@gmail.com>
2018-03-25 21:32:03 -0300

    melhoria no script de avaliacao

--- trab01/tests_client.lua
+++ trab01/tests_client.lua
@@ -2,12 +2,13 @@ dofile("client1.lua")
 
 print("#\tnumReq\tseconds")
 
-local numReq = 10000
-while numReq <= 100000 do
+local step = 10^5
+local numReq = step
+while numReq <= step * 10 do
     local e = 0
     while e < 3 do
         print(e.."\t"..numReq.."\t"..client(numReq))
         e = e + 1
     end
-    numReq = numReq + 10000
+    numReq = numReq + step
 end
